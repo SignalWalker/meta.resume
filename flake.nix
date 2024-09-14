bc361cdf367239653659d10bc15b94d5c41f2844
Ash Walker <ashurstwalker@gmail.com>
2024-09-14 11:13:29 -0400

    feat: build with flake

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    alejandra = {
+      url = "github:kamadorueda/alejandra";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    ...
+  }:
+    with builtins; let
+      std = nixpkgs.lib;
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      nixpkgsFor = std.genAttrs systems (system:
+        import nixpkgs {
+          localSystem = builtins.currentSystem or system;
+          crossSystem = system;
+          overlays = [];
+        });
+      stdenvFor = pkgs: pkgs.stdenvAdapters.useMoldLinker pkgs.llvmPackages_latest.stdenv;
+    in {
+      formatter = std.mapAttrs (system: pkgs: pkgs.default) inputs.alejandra.packages;
+      packages =
+        std.mapAttrs (system: pkgs: let
+          std = pkgs.lib;
+          stdenv = stdenvFor pkgs;
+        in {
+          "ashwalker-resume" = pkgs.buildNpmPackage {
+            pname = "ashwalker-resume";
+            version = "1.0.0";
+            src = ./.;
+            nodejs = pkgs.nodejs_18;
+            npmBuildScript = "resumed";
+            npmBuildFlags = ["render" "$src/resume.json" "-o" "$out/resume.html"];
+          };
+        })
+        nixpkgsFor;
+      devShells =
+        std.mapAttrs (system: pkgs: let
+          selfPkgs = self.packages.${system};
+          stdenv = stdenvFor pkgs;
+        in {
+          default = (pkgs.mkShell.override {inherit stdenv;}) {
+            stdenv = stdenvFor.${system};
+            inputsFrom = [selfPkgs.default];
+          };
+        })
+        nixpkgsFor;
+    };
+}
